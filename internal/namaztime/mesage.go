db76d5bb56ed5d9a42ecc4e528081bf17baf9b78
Isa Nazhmudinov <i.nazhmudinov@corp.mail.ru>
2022-08-04 20:53:02 +0300

    add getDeclensionByNumber method

--- internal/namaztime/mesage.go
+++ internal/namaztime/mesage.go
@@ -3,28 +3,37 @@ package namaztime
 import (
 	"bytes"
 	"fmt"
+	"strconv"
 	"text/template"
 	"time"
 )
 
 const (
-	namazTimeMessageTemplate    = "До {{.Description}} намаза {{.TitleRu}} в {{.Time}}, осталось {{.TimeLeft}}"
-	namazTimeMessageTemplateTTS = "До {{.Description}} намаза {{.TTS}} в {{.Time}}, осталось {{.TimeLeft}}"
+	namazTimeMessageTemplate    = "До {{.Description}} намаза {{.TitleRu}} в {{.Time}}, {{.LeftText}} {{.TimeLeft}}"
+	namazTimeMessageTemplateTTS = "До {{.Description}} намаза {{.TTS}} в {{.Time}}, {{.LeftText}} {{.TimeLeft}}"
+)
+
+var (
+	declensionHours   = []string{"час", "часа", "часов"}
+	declensionMinutes = []string{"минута", "минуты", "минут"}
+	declensionLeft    = []string{"Остался", "Осталось", "Осталось"}
 )
 
 type TextDto struct {
-	Title       string
-	TitleRu     string
 	Description string
+	LeftText    string
+	TTS         string
 	Time        string
 	TimeLeft    string
-	TTS         string
+	Title       string
+	TitleRu     string
 }
 
 func (n *TextDto) New(title string, time time.Time, timeLeft time.Duration) *TextDto {
 	namazTime := TextsMap[title]
 	namazTime.TimeLeft = fmtDurationToText(timeLeft)
 	namazTime.Time = time.Format("15:04")
+	namazTime.LeftText = getDeclensionByNumber(time.Hour(), declensionLeft)
 
 	return namazTime
 }
@@ -35,24 +44,40 @@ func fmtDurationToText(d time.Duration) string {
 	d -= h * time.Hour
 	m := d / time.Minute
 
-	mText := "минут"
-	if m == 1 {
-		mText = "минута"
-	} else if m <= 4 {
-		mText = "минуты"
-	}
-
-	hText := "часов"
-	if h == 1 {
-		hText = "час"
-	} else if h <= 4 {
-		hText = "часа"
-	}
+	mText := getDeclensionByNumber(int(m), declensionMinutes)
+	hText := getDeclensionByNumber(int(h), declensionHours)
 
 	if h <= 0 {
 		return fmt.Sprintf("%d %s", m, mText)
 	}
 	return fmt.Sprintf("%d %s и %d %s", h, hText, m, mText)
+}
+
+func getDeclensionByNumber(number int, declensionForms []string) string {
+	if len(declensionForms) < 3 {
+		return ""
+	}
+
+	numberRune := []rune(strconv.Itoa(number))
+	numberLen := len(numberRune)
+	lastOneSymbol, _ := strconv.Atoi(string(numberRune[numberLen-1 : numberLen]))
+
+	if numberLen > 1 {
+		lastTwoSymbol, _ := strconv.Atoi(string(numberRune[numberLen-2 : numberLen]))
+		if lastTwoSymbol >= 10 && lastTwoSymbol <= 20 {
+			return declensionForms[2]
+		}
+	}
+
+	if lastOneSymbol == 0 {
+		return declensionForms[2]
+	} else if lastOneSymbol < 2 {
+		return declensionForms[0]
+	} else if lastOneSymbol < 5 {
+		return declensionForms[1]
+	} else {
+		return declensionForms[2]
+	}
 }
 
 func getTextByTextDtoAndTemplate(namazTextDto *TextDto, textTemplate string) (*string, error) {
@@ -73,43 +98,48 @@ func getTextByTextDtoAndTemplate(namazTextDto *TextDto, textTemplate string) (*s
 
 var TextsMap = map[string]*TextDto{
 	Fajr: {
+		Description: "утреннего",
+		LeftText:    "",
+		TTS:         "Фаджр",
+		Time:        "",
+		TimeLeft:    "",
 		Title:       "Fajr",
 		TitleRu:     "Фаджр",
-		TTS:         "Фаджр",
-		Description: "утреннего",
-		Time:        "",
-		TimeLeft:    "",
 	},
 	Dhuhr: {
+		Description: "обеденного",
+		LeftText:    "",
+		TTS:         "Зухр",
+		Time:        "",
+		TimeLeft:    "",
 		Title:       "Dhuhr",
 		TitleRu:     "Зухр",
-		TTS:         "Зухр",
-		Description: "обеденного",
-		Time:        "",
-		TimeLeft:    "",
 	},
 	Asr: {
+		Description: "послеобеденного",
+		LeftText:    "",
+		TTS:         "`Аср",
+		Time:        "",
+		TimeLeft:    "",
 		Title:       "Asr",
 		TitleRu:     "Аср",
-		TTS:         "`Аср",
-		Description: "послеобеденного",
-		Time:        "",
-		TimeLeft:    "",
 	},
 	Maghrib: {
+		Description: "вечернего",
+		LeftText:    "",
+		TTS:         "М`агриб",
+		Time:        "",
+		TimeLeft:    "",
 		Title:       "Maghrib",
 		TitleRu:     "Магриб",
-		TTS:         "М`агриб",
-		Description: "вечернего",
-		Time:        "",
-		TimeLeft:    "",
 	},
 	Isha: {
-		Title:       "Isha",
-		TitleRu:     "Иша",
-		TTS:         "`Иша",
 		Description: "Ночного",
+		LeftText:    "",
+		TTS:         "`Иша",
 		Time:        "",
 		TimeLeft:    "",
+		Title:       "Isha",
+		TitleRu:     "Иша",
 	},
 }
